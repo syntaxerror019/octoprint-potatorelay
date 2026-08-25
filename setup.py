43ece159ba05d1e9dbe59fe5c5fc65af29944286
Miles Hilliard <skywiredvt@gmail.com>
2026-08-25 12:51:04 -0400

    im praying

--- setup.py
+++ setup.py
@@ -15,26 +15,34 @@ plugin_author = "Miles Hilliard"
 plugin_author_email = "miles@mileshilliard.com"
 plugin_url = "https://github.com/syntaxerror019/octoprint-potatorelay"
 plugin_license = "AGPLv3"
-plugin_requires = [""]
+plugin_requires = []
 
 plugin_additional_data = []
 plugin_additional_packages = []
 plugin_additional_stuff = []
 
-setup(
+try:
+    import octoprint_setuptools
+except ImportError:
+    print("Could not import OctoPrint's setuptools, are you sure you are running that under "
+          "the same python installation that OctoPrint is installed under?")
+    import sys
+    sys.exit(-1)
+
+setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
+    identifier=plugin_identifier,
+    package=plugin_package,
     name=plugin_name,
     version=plugin_version,
     description=plugin_description,
     author=plugin_author,
-    author_email=plugin_author_email,
+    mail=plugin_author_email,
     url=plugin_url,
     license=plugin_license,
-    packages=[plugin_package],
-    include_package_data=True,
-    install_requires=plugin_requires,
-    entry_points={
-        "octoprint.plugin": [
-            "{} = {}".format(plugin_identifier, plugin_package)
-        ]
-    },
+    requires=plugin_requires,
+    additional_packages=plugin_additional_packages,
+    ignored_packages=[],
+    additional_data=plugin_additional_data,
 )
+
+setup(**setup_parameters)
